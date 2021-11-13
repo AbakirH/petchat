db9d0bf403d8bec65e599e292ef3e49298bf9bae
GianniGabriel <giannigab789@gmail.com>
2021-11-13 15:54:10 -0500

    Change messages

--- send-sms.js
+++ send-sms.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken); 
- 
-client.messages 
-      .create({         
-         to: '+12014554728', 
-         from: '+14253828517',
-         body: "Hello I am your best friend"
-       }) 
-      .then(message => console.log(message.sid)) 
-      .done();
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    to: "+12014554728",
+    from: "+14253828517",
+    body: "Hello I am your best friend",
+  })
+  .then((message) => console.log(message.sid))
+  .done();
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-require('dotenv');
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+require("dotenv");
+const http = require("http");
+const express = require("express");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 const app = express();
 
-app.post('/sms', (req, res) => {
+app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
+  twiml.message("Hi, Abakir, I am your pet!");
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
 
 http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
+  console.log("Express server listening on port 1337");
 });
